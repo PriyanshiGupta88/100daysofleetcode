128df4b88f6795240c43cbb0d7f2545d31d79d10
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-01 12:31:57 +0530

    Create 3(iiI).py

---
+++ https:/leetcode.com/problems/base-7/3(iiI).py
@@ -0,0 +1,12 @@
+num=int(input())
+if num == 0:    return "0"
+        s = ""
+        if num < 0:
+            f = -1
+        else:
+            f = 1
+        num = abs(num)
+        while num:
+            s += str(num % 7)
+            num //=7
+        return s[::-1] if f == 1 else '-' + s[::-1]
