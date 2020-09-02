f162e156da18d7578f33d4e8b6016f14623cb8d2
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-02 21:01:03 +0530

    Create 4(ii).py

---
+++ https:/leetcode.com/problems/contains-duplicate-ii/4(ii).py
@@ -0,0 +1,8 @@
+if(len(nums)==len(set(nums))):
+            return False
+        for i in range(len(nums)):
+            if(i<i+k+1):
+                f=nums[i+1:i+k+1]
+                if(nums[i] in f):
+                    return True
+        return False
