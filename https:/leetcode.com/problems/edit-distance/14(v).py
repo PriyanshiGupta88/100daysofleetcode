6f23e4d9009b570851496feb457b5cae091456f2
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-21 17:52:04 +0530

    Update 14(v).py

--- https:/leetcode.com/problems/edit-distance/14(v).py
+++ https:/leetcode.com/problems/edit-distance/14(v).py
@@ -4,27 +4,17 @@ class Solution:
         n=len(str2)
         dp = [[0 for x in range(n + 1)] for x in range(m + 1)] 
 
-        # Fill d[][] in bottom up manner 
         for i in range(m + 1): 
             for j in range(n + 1): 
 
-                # If first string is empty, only option is to 
-                # insert all characters of second string 
+                
                 if i == 0: 
-                    dp[i][j] = j    # Min. operations = j 
-
-                # If second string is empty, only option is to 
-                # remove all characters of second string 
+                    dp[i][j] = j    
                 elif j == 0: 
-                    dp[i][j] = i    # Min. operations = i 
-
-                # If last characters are same, ignore last char 
-                # and recur for remaining string 
+                    dp[i][j] = i    
                 elif str1[i-1] == str2[j-1]: 
                     dp[i][j] = dp[i-1][j-1] 
 
-                # If last character are different, consider all 
-                # possibilities and find minimum 
                 else: 
                     dp[i][j] = 1 + min(dp[i][j-1],        # Insert 
                                        dp[i-1][j],        # Remove 
