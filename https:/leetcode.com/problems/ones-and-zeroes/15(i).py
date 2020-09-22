89f98d45296ae23865d320511a706c3d84b3b97c
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-22 11:57:54 +0530

    Update 15(i).py

--- https:/leetcode.com/problems/ones-and-zeroes/15(i).py
+++ https:/leetcode.com/problems/ones-and-zeroes/15(i).py
@@ -1,4 +1,4 @@
-import numpy as np
+"""import numpy as np
 class Solution:
     def findMaxForm(self, a: List[str], m: int, n: int) -> int:
         dp = [[0] * (n+1)  for _ in range(m+1)]        
@@ -7,4 +7,16 @@ class Solution:
             for j in range(m+1)[::-1]:
                 for k in range(n+1)[::-1]:                                        
                     dp[j][k] = max(dp[j][k], dp[j-c['0']][k-c['1']] + 1 if c['0'] <= j and c['1'] <= k else 0)                                 
+        return dp[m][n]
+"""
+import numpy as np
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [ [0]*(n+1) for _ in range(m+1) ]   # [0][1]
+        
+        for ones, zeros in [ (s.count('1'), s.count('0')) for s in strs]:
+            for i in range(m, zeros-1, -1):
+                for j in range(n, ones-1, -1):
+                    dp[i][j] = max([ dp[i][j], dp[i-zeros][j-ones]+1 ])
+
         return dp[m][n]
