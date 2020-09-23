9f4c7c8627205e72ce951617151ec34721d67de5
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-23 14:48:00 +0530

    Create 15(iv).py

---
+++ https:/leetcode.com/problems/length-of-longest-fibonacci-subsequence/15(iv).py
@@ -0,0 +1,24 @@
+class Solution:
+    def lenLongestFibSubseq(self, A: List[int]) -> int:
+       # a hash table 
+        S = set(A) 
+        maxLen = 0
+        n=len(A)
+        for i in range(0, n): 
+            for j in range(i + 1, n): 
+
+                x = A[j] 
+                y = A[i] + A[j] 
+                length = 2
+
+                
+                while y in S: 
+
+                    
+                    z = x + y 
+                    x = y 
+                    y = z
+                    length += 1
+                    maxLen = max(maxLen, length) 
+
+        return maxLen if maxLen >= 3 else 0
