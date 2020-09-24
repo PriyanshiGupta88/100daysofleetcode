1a6e5da8f505fb16c6df790d70f7309661eba329
Priyanshi gupta <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-09-24 10:06:39 +0530

    Create 16(iii).py

---
+++ https:/www.hackerrank.com/challenges/fibonacci-modified/problem/16(iii).py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    a=t1
+    b=t2
+    if n==0:
+        return t1
+    if n==1:
+        return t2
+    for i in range(2,n):
+        c=t1+(t2**2)
+        t1=t2
+        t2=c
+    return c
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
